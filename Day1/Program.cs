875485a690ea3037c40e86009cf6aed581741652
Wojtek <wojtek.kajstura@gmail.com>
2020-12-01 23:22:13 +0100

    day1 done

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
 
-namespace Day1
+var allLinesOfFile = File.ReadAllLines("../../../input.txt");
+
+var overPartOne = false;
+var overPartTwo = false;
+
+foreach (var numInString in allLinesOfFile)
 {
-    class Program
+    var number = Convert.ToInt32(numInString);
+
+    foreach (var numInString2 in allLinesOfFile)
     {
-        static void Main(string[] args)
+        var number2 = Convert.ToInt32(numInString2);
+
+        if (number + number2 == 2020 && !overPartOne)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"part one answer, it's: {number} + {number2} = 2020");
+            Console.WriteLine($"multiplied: {number} * {number2} = {number * number2}");
+            overPartOne = true;
+        }
+
+        foreach (var numInString3 in allLinesOfFile)
+        {
+            var number3 = Convert.ToInt32(numInString3);
+
+            if (number + number2 + number3 == 2020 && !overPartTwo)
+            {
+                Console.WriteLine($"part two answer, it's: {number} + {number2} + {number3} = 2020");
+                Console.WriteLine($"multiplied: {number} * {number2} * {number3} = {number * number2 * number3}");
+                overPartTwo = true;
+            }
         }
     }
 }
